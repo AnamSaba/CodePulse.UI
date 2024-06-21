399625ffd2ed9ec6d8ebb034207afbd6842eee85
anamsaba18 <anam.saba378@gmail.com>
2024-06-20 19:01:59 -0700

    Edit-Category component implementation done.

--- codepulse.ui/src/app/features/category/services/category.service.ts
+++ codepulse.ui/src/app/features/category/services/category.service.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { AddCategoryRequest } from '../model/add-category-request.model';
 import { Category } from '../model/category.model';
 import { environment } from 'src/environments/environment';
+import { UpdateCategoryResquest } from '../model/update-category-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,14 @@ export class CategoryService {
 
   getAllCategory() : Observable<Category[]>{
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
+  }
+
+  getCategoryById(id: string) : Observable<Category>{
+    return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
+
+  updateCategory(id:string, updateCategoryRequest: UpdateCategoryResquest) : Observable<Category> {
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`,updateCategoryRequest)
   }
 
 }
